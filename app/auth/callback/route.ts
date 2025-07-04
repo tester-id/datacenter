7de0c734a12784eb972e8a519daf1c50c1e9cc54
Tester ID <unknown.tester2779@gmail.com>
2025-07-04 22:07:00 +0800

    changes

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { type CookieOptions, createServerClient } from '@supabase/ssr'
